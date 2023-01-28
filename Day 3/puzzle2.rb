86cbacd8b4d3c3275c97d05d42d6436a057258fc
Ashley Boehs <ashley@boehs.com>
2023-01-28 13:02:45 -0600

    Solution for Puzzle 2, Day 3

--- Day 3/puzzle2.rb
+++ Day 3/puzzle2.rb
@@ -1,19 +1,13 @@
-require 'pry'
-
 rucksacks = File.readlines 'input.txt', chomp: true
-
+elf_groups =  rucksacks.each_slice(3).map{|a| a.join(" ")}
 total = 0
-rucksacks.each do |rucksack|
-  middle = rucksack.length / 2
-  first_compartment = rucksack[0...middle].chars
-  last_compartment = rucksack[middle..].chars
-  duplicates = first_compartment & last_compartment
-  binding.pry
-  first_compartment + last_compartment + first_compartment = rucksacks.each_slice(3).map{|a| a.join("first_compartment + last_compartment + first_compartment ")}
+
+elf_groups.each do |elf_group|
+  elf1, elf2, elf3 = elf_group.split
+  duplicates = elf1.chars & elf2.chars & elf3.chars
 
   priority_map = ("a".."z").to_a + ("A".."Z").to_a
   total = total + priority_map.index(duplicates.first) + 1
-  
-  
 end
+
 puts total
