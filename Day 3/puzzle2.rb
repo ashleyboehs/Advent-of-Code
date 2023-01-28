7cb2ccd7acaa82fa4ec485a46f6dc3f5cf9785f5
Ashley Boehs <ashley@boehs.com>
2023-01-28 11:51:25 -0600

    WIP Day 3 (syncing to laptop)

--- Day 3/puzzle2.rb
+++ Day 3/puzzle2.rb
@@ -1,14 +1,19 @@
+require 'pry'
+
 rucksacks = File.readlines 'input.txt', chomp: true
 
+total = 0
 rucksacks.each do |rucksack|
   middle = rucksack.length / 2
   first_compartment = rucksack[0...middle].chars
   last_compartment = rucksack[middle..].chars
   duplicates = first_compartment & last_compartment
+  binding.pry
+  first_compartment + last_compartment + first_compartment = rucksacks.each_slice(3).map{|a| a.join("first_compartment + last_compartment + first_compartment ")}
 
-  total = 0  
   priority_map = ("a".."z").to_a + ("A".."Z").to_a
   total = total + priority_map.index(duplicates.first) + 1
   
-  puts total
+  
 end
+puts total
